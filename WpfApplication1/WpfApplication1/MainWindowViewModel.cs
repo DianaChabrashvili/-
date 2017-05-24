47dfea5332f3567ec2935ce4ca081499dc0ab248
Диана Чабрашвили <firegear@bk.ru>
2017-05-25 00:01:04 +0300

    биндинг

---
+++ WpfApplication1/WpfApplication1/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    public class MainWindowViewModel
+    {
+        public string avtor1 { get; set; }
+        public string avtor2 { get; set; }
+        public string novavtor { get; set; }
+        public string namebook { get; set; }
+        public string nameizdatel { get; set; }
+        public string nameyear { get; set; }
+        public int tiraj { get; set; }
+        public string isbn { get; set; }
+        public ICommand save { get; set; }
+        public ICommand otmena { get; set; }
+
+
+
+
+    }
+}
