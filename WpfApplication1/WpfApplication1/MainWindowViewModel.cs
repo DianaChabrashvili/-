6d2dc3c376dd2d94e8362e796b4940c4f279be35
Диана Чабрашвили <firegear@bk.ru>
2017-06-01 00:25:55 +0300

    Lab4

--- WpfApplication1/WpfApplication1/MainWindowViewModel.cs
+++ WpfApplication1/WpfApplication1/MainWindowViewModel.cs
@@ -1,27 +1,67 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.ComponentModel;
 
 namespace WpfApplication1
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
         public string avtor1 { get; set; }
         public string avtor2 { get; set; }
         public string novavtor { get; set; }
         public string namebook { get; set; }
         public string nameizdatel { get; set; }
-        public string nameyear { get; set; }
-        public int tiraj { get; set; }
+        public int _nameyear { get; set; }
+        public int _tiraj { get; set; }
         public string isbn { get; set; }
         public ICommand save { get; set; }
         public ICommand otmena { get; set; }
 
+        public int nameyear
+        {
+            get { return _nameyear; }
+            set
+            {
+                if (value > 0)
+                {
+                    _nameyear = value;
+                    Notify(nameof(nameyear));
+                }
+                else
+                {
+
+                }
+            }
+        }
 
 
+        public int tiraj
+        {
+            get { return _tiraj; }
+            set
+            {
+                if (value > 0)
+                {
+                    _tiraj = value;
+                    Notify(nameof(tiraj));
+                }
+                else
+                {
 
+                }
+            }
+        }
+
+        private void Notify(string v)
+        {
+            //  throw new NotImplementedException(v);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
